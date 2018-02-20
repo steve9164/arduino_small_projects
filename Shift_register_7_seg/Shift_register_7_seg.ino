d27d3b6ea1edd25c4555c63a95a6cffbf627dbb3
Stephen Davies <steve9164@gmail.com>
2018-02-20 22:27:20 +1100

    Update 7-segment to support 2 multiplexed digits from one shift register using PNP transistors

--- Shift_register_7_seg/Shift_register_7_seg.ino
+++ Shift_register_7_seg/Shift_register_7_seg.ino
@@ -1,8 +1,26 @@
+// Control 2 common anode 7-segment digits with one shift register, one bank of 8 resistors
+// and 2 PNP transistors installed between 5V and the anodes of the 7-segments
+
 // Shift register pins
 #define LATCH (6)
 #define DATA  (4)
 #define CLOCK (5)
+#define TR1   (7)
+#define TR2   (8)
 
+// Inverted (use with common anode 7-segment)
+#define ZERO    (0b00000011)
+#define ONE     (0b10011111)
+#define TWO     (0b00100101)
+#define THREE   (0b00001101)
+#define FOUR    (0b10011001)
+#define FIVE    (0b01001001)
+#define SIX     (0b01000001)
+#define SEVEN   (0b00011111)
+#define EIGHT   (0b00000001)
+#define NINE    (0b00001001)
+
+const int digits[] = {ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE};
 
 void shiftNumber(byte num) {
   // Shift a byte to the shift register
@@ -16,14 +34,32 @@ void setup() {
   pinMode(LATCH, OUTPUT);
   pinMode(DATA, OUTPUT);
   pinMode(CLOCK, OUTPUT);
+  pinMode(TR1, OUTPUT);
+  pinMode(TR2, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 }
 
+unsigned long t;
 void loop() {
   // put your main code here, to run repeatedly:
-  for (int i = 0; i < 256; i++) {
-    shiftNumber(i);
-    digitalWrite(LED_BUILTIN, i % 2 ? HIGH : LOW);
-    delay(100);
+  for (int d1 = 0; d1 < 10; d1++) {
+    for (int d2 = 0; d2 < 10; d2++) {
+      t = millis();
+      while (millis() < t + 200) {
+        // Turn off TR2
+        digitalWrite(TR2, HIGH);
+        shiftNumber(digits[d1]);
+        // Activate TR1
+        digitalWrite(TR1, LOW);
+        delay(3);
+
+        digitalWrite(TR1, HIGH);
+        shiftNumber(digits[d2]);
+        // Activate TR2
+        digitalWrite(TR2, LOW);
+        delay(3);
+
+      }
+    }
   }
 }
